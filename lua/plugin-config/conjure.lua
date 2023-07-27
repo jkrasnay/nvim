0fae248bbc5f5fa4f9ecfb854bb4b3ae8045cbe6
John Krasnay <john@krasnay.ca>
2023-07-26 23:36:23 -0400

    Disable lsp in Conjure log buffer

--- lua/plugin-config/conjure.lua
+++ lua/plugin-config/conjure.lua
@@ -4,3 +4,12 @@ vim.g['conjure#client#clojure#nrepl#completion#with_context'] = false
 
 -- don't map K, since we use LSP instead
 vim.g['conjure#mapping#doc_word'] = false
+
+-- Disable LSP diagnostics in the Conjure log buffer
+--
+-- (vim.diagnostic.disable takes an optional argument which is the number of
+-- the buffer to disable diagnostics in. 0 means the current buffer.)
+--  autocmd BufEnter conjure-log-* lua vim.diagnostic.disable(0)
+vim.api.nvim_create_autocmd(
+  { "BufEnter" },
+  { pattern = "conjure-log-*", command = "lua vim.diagnostic.disable(0)" })
