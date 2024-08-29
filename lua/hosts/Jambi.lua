2e07253b84757986bad0a270aa6e379cb6373045
John Krasnay <john@krasnay.ca>
2024-08-28 22:11:07 -0400

    Rename MacBook-Air to Jambi

--- lua/hosts/Jambi.lua
+++ lua/hosts/Jambi.lua
@@ -1,9 +1,17 @@
 require('erbium')
 
+vim.o.guifont = 'FiraCode Nerd Font:h16'
+vim.g.neovide_cursor_vfx_mode = 'railgun'
+
 vim.g.projects = {
+  '~/ws/diascript',
   '~/ws/erbium',
+  '~/ws/erbium-config',
   '~/ws/erbium-db',
-  '~/ws/retab',
+  '~/ws/erbium-docs',
+  '~/ws/erbium-kiosk',
+  '~/ws/retab-3',
+  '~/ws/signamo',
   '~/ws/sundog-tools',
   '~/ws/the-little-typer',
   '~/Dropbox/sundog-consulting',
@@ -16,3 +24,5 @@ vim.g.notes_dir = '~/Dropbox/Notes'
 vim.g.jdtls_path = '/usr/local/opt/jdtls-1.29.0'
 vim.g.jdtls_java_path = '/Users/john/.sdkman/candidates/java/current/bin/java'
 vim.g.jdtls_workspaces_dir = '/Users/john/.local/state/jdtls'
+
+-- vim:set ft=lua:
