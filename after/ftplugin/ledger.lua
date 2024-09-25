73ae4cb868e10f19ed81654cd4ce45655df7a93b
John Krasnay <john@krasnay.ca>
2024-09-25 08:54:34 -0400

    Align ledger files on save

--- after/ftplugin/ledger.lua
+++ after/ftplugin/ledger.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '==', function() vim.cmd('LedgerAlign') end, { buffer = true })
 
-vim.api.nvim_create_autocmd("BufWrite", {
+vim.api.nvim_create_autocmd("Bufrite", {
   buffer = 0,
   command = 'LedgerAlignBuffer',
 })
