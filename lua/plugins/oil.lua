f64fd09886a41ac584d2af833f91285001bf3f47
John Krasnay <john@krasnay.ca>
2024-09-25 08:56:17 -0400

    <leader>os to open current dir in split

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,8 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   keys = {
-    { '<leader>oo', function() require('oil').open() end, desc = '[O]pen this file\'s dir' },
+    { '<leader>oo', function() require('oil').open() end, desc = '[O]pen current dir' },
     { '<leader>op', function() require('oil').open(vim.fn.getcwd(-1,0)) end, desc = '[O]pen [P]arent dir' },
+    { '<leader>os', function() vim.cmd.split(); require('oil').open() end, desc = '[O]pen current dir in split' },
   },
 }
